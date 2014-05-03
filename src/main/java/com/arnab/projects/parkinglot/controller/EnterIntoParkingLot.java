5712fea00944d2491a9c89973867db27130a2dd6
arnab <carn76@gmail.com>
2014-05-03 12:17:25 -0400

    Adding a documentation note

--- src/main/java/com/arnab/projects/parkinglot/controller/EnterIntoParkingLot.java
+++ src/main/java/com/arnab/projects/parkinglot/controller/EnterIntoParkingLot.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+/*
+ Get a parking slip printed on entering the parking lot
+ */
 @RestController
 public class EnterIntoParkingLot {
 

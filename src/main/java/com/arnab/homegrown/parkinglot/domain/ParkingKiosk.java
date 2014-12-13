a6451898b8ac8e86b2828f61b7069548add05b9e
Arnabh Chakravarty <carn76@gmail.com>
2014-12-13 13:30:33 -0600

    Refactorings

--- src/main/java/com/arnab/homegrown/parkinglot/domain/ParkingKiosk.java
+++ src/main/java/com/arnab/homegrown/parkinglot/domain/ParkingKiosk.java
@@ -2,10 +2,9 @@ package com.arnab.homegrown.parkinglot.domain;
 
 import com.arnab.homegrown.parkinglot.domain.vo.EntryParkingSlip;
 import com.arnab.homegrown.parkinglot.service.exception.ParkingSlipPrintingException;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
-@Resource
+@Service
 public interface ParkingKiosk {
 
     EntryParkingSlip printNewParkingSlip() throws ParkingSlipPrintingException;
